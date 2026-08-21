b793ce7c1986f9bf12ab7f42549f01849f64c348
Alec Hajinoff <hajinoff@gmail.com>
2026-08-21 06:53:40 +0100

    21/8/26, 6:53

--- React/src/ShopPage.js
+++ React/src/ShopPage.js
@@ -46,7 +46,6 @@ function ShopPage() {
 
   return (
     <div className="container my-4">
-      <h2 className="mb-4">Shop Products & Services</h2>
       <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
         {products.map((product) => (
           <div className="col" key={product.id}>
