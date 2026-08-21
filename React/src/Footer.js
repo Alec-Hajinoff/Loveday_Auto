ab247b60b826c62fb26bd18943f29f50865e05f8
Alec Hajinoff <hajinoff@gmail.com>
2026-08-21 17:19:15 +0100

    21/8/26, 17:19

--- React/src/Footer.js
+++ React/src/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
@@ -7,20 +6,21 @@ function Footer() {
   const user = ["i", "n", "f", "o"].join("");
   const domain = ["lovedayauto", "com"].join(".");
   const email = `${user}@${domain}`;
+
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col-12">
-          <p className="footer">
-            <em>
+    <footer className="footer-wrapper">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-12">
+            <p className="footer-content">
               &copy; Copyright 2025 - {currentYear}. Address: 50A Southbury Rd,
               Enfield, EN1 1YB. Phone: 020 8367 5888. Email:{" "}
               <a href={`mailto:${email}`}>{email}</a>
-            </em>
-          </p>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
