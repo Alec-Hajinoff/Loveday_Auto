e957c3531749274d2fce54673ae0226b47f174d3
Alec Hajinoff <hajinoff@gmail.com>
2026-09-19 14:27:04 +0100

    19/9/26, 14:27

--- React/src/Footer.js
+++ React/src/Footer.js
@@ -12,11 +12,25 @@ function Footer() {
       <div className="container text-center">
         <div className="row">
           <div className="col-12">
-            <p className="footer-content">
+            <p className="footer-content mb-2">
               &copy; Copyright 2025 - {currentYear}. Garage address: 50a
               Southbury Rd, Enfield, EN1 1YB. Phone: 020 8367 5888. Email:{" "}
               <a href={`mailto:${email}`}>{email}</a>
             </p>
+
+            <div className="footer-builder-wrapper">
+              <span className="footer-builder-text">
+                Web application by:{" "}
+                <a
+                  href="https://hertfordstandard.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="footer-builder-link"
+                >
+                  Hertford Standard
+                </a>
+              </span>
+            </div>
           </div>
         </div>
       </div>
