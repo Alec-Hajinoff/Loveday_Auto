b4bd173b76fd790ebd703ec74ed4fc6c4666634d
Alec Hajinoff <hajinoff@gmail.com>
2026-09-20 10:35:54 +0100

    20/9/26, 10:35

--- React/src/AboutUs.js
+++ React/src/AboutUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AboutUsStory from "./AboutUsStory";
 import "./AboutUs.css";
 
 function AboutUs() {
@@ -18,23 +19,7 @@ function AboutUs() {
         </div>
       </section>
 
-      <div className="container about-us-container">
-        <div className="row">
-          <div className="col-12">
-            <h2 className="about-us-title">About Loveday Auto Repairs</h2>
-            <p className="about-us-subtitle">
-              Learn more about our garage history, team, certifications, and
-              licenses. Full business details coming soon.
-            </p>
-            <div className="about-us-placeholder-box">
-              <p className="mb-0">
-                Business information and certification data are currently
-                pending integration.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <AboutUsStory />
     </div>
   );
 }
