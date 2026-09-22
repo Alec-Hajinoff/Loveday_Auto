37c6b683e4307594a6a43bf4e880d67310c8cb60
Alec Hajinoff <hajinoff@gmail.com>
2026-09-22 09:31:15 +0100

    22/9/26, 9:31

--- React/src/BusinessHoursManager.js
+++ React/src/BusinessHoursManager.js
@@ -90,7 +90,7 @@ function BusinessHoursManager() {
 
   return (
     <div className="business-hours-container">
-      <h5>Business Hours</h5>
+      <h5>Add Opening Hours</h5>
       <form onSubmit={handleSubmit}>
         <div className="business-hours-layout">
           <div className="business-hours-form-side">
